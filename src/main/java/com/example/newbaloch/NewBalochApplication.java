8d1eafeaa778f5aa42d5b9cdd66af6e8f9aaf175
wahag-baloch <wahagnoorbaloch@gmail.com>
2022-03-16 12:37:10 +0500

    hello wahag

---
+++ src/main/java/com/example/newbaloch/NewBalochApplication.java
@@ -0,0 +1,13 @@
+package com.example.newbaloch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewBalochApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewBalochApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/newbaloch/NewBalochApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.newbaloch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewBalochApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
